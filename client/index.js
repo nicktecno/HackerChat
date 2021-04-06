a40a2cd4826839f4580ff600163721ba36d20eb1
nicktecno <nick.tecno@gmail.com>
2021-04-06 12:35:40 -0300

    HackerChat

---
+++ client/index.js
@@ -0,0 +1,13 @@
+import Events from 'events';
+import CliConfig from './src/cliConfig.js';
+import SocketClient from './src/socket.js'
+import TerminalController from "./src/terminalController.js";
+
+const [nodePath, filePath, ...commands] = process.argv
+const config = CliConfig.parseArguments(commands)
+
+const componentEmitter = new Events()
+const socketClient = new SocketClient(config)
+await socketClient.initialize()
+// const controller = new TerminalController()
+// await controller.initializeTable(componentEmitter)
